6086751638cba57cc85a0912eb81ba01328c7a0d
Vitor <v.augusto.am@gmail.com>
2022-10-02 23:20:27 -0300

    att

--- src/controller/estudantesController.js
+++ src/controller/estudantesController.js
@@ -1,71 +1,47 @@
-import { Database } from '../infra/database.js'
+import ValidationService from '../service/validationService.js'
+import DbMethods from '../DAO/databaseMethods.js'
+import StudentModel from '../model/studentsModel.js'
 
-class DbMethods {
-  static createTable() {
-    const query = `
-      CREATE TABLE IF NOT EXISTS students(
-      id_student INTEGER PRIMARY KEY AUTOINCREMENT,
-      name_student VARCHAR,
-      class_student VARCHAR,
-      age_student INTERGER
-      )
-    `;
-    return new Promise((resolve, reject) => {
-      Database.run(query, (erro) => {
-        if (erro) {
-          reject(erro.message);
-        } else {
-          resolve("Successfully creating students table!");
-        }
-      });
+DbMethods.createTable()
+
+class studentsController {
+  static rotas(app){
+    app.get("/", async (req, res) => {
+      try {
+        const response = await DbMethods.getStudents()
+        res.status(200).json(response)
+      } catch (error) {
+        res.status(400).send(error)
+      }  
     });
-  }
 
-  static getStudents() {
-    const query = `SELECT * FROM students`
-
-    return new Promise((resolve, reject)=> {
-      Database.all(query, (e, result)=>{
-          if(e){
-              reject(e.message)
-          } else {
-              resolve(result)
-          }
-      })
+    app.post("/", async (req, res) => {
+      try {
+        const body = req.body
+        const itemValid = ValidationService.validNewStudent(body.name_student, body.class_student, body.age_student)
+        if (itemValid) {
+          const item = new StudentModel(...Object.values(req.body))
+          const response = await DbMethods.postStudent(item)
+          res.status(201).json(response)
+        } else {
+          res.status(401).json("Check the item. Object not registered.")
+        }
+      } catch  (error) {
+        res.status(401).send({error:error.message})
+      }      
     })
-  }
 
-  static postStudent(item) {
-
-    const query = `INSERT INTO students (name_student, class_student, age_student) VALUES (?,?,?)`
-
-    const body = Object.values(item)
-
-      return new Promise((resolve, reject)=>{
-        Database.run(query, [...body], (e)=>{
-          if(e){
-            reject(e.message)
-          } else {
-            resolve({message: "Registered successfully!"})
-          }
-        })
-      })
-    
-  }
-
-  static deletStudent(id) {
-    const query = `DELETE FROM students WHERE id_student = ?`
-    return new Promise((resolve, reject) => {
-      Database.run(query, id, (e)=>{
-          if(e){
-              reject(e.message)
-          } else {
-              resolve({message: `Record with Id ${id} successfully deleted.`})
-          }
-      })
+    app.delete("/:id", async (req, res) => {
+      try {                
+        const item = await DbMethods.deletStudent(req.params.id)
+        res.status(200).json(item)
+      } catch (error) {    
+        res.status(404).json({Error: error.message})
+      }
     })
+    ;
   }
 
-}
+};
 
-export default DbMethods;
+export default StudentsController;
